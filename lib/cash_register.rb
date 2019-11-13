9b2174d98e27bdbabb6b59cf564afc9d8679d409
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 04:33:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    self.last_price = price
+    self.last_price = price * quantity
     self.items += [title] * quantity
     self.total += price * quantity
   end
