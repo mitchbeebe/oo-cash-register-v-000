d47fffc14288f512d2f68f62c724361605566967
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 04:32:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,16 @@
 class CashRegister
   
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :last_price
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
+    @last_price = 0
   end
   
   def add_item(title, price, quantity = 1)
+    self.last_price = price
     self.items += [title] * quantity
     self.total += price * quantity
   end
@@ -24,6 +26,8 @@ class CashRegister
   
   def void_last_transaction
     self.items.pop
+    self.total -= self.last_price
+  end
   
   
   
